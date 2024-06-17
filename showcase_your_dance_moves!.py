e17a4a139ea8c0404cdb7c57c736428b82973b38
JayAnders1 <justinjay615@gmail.com>
2024-06-16 20:07:48 -0400

    first commit

---
+++ showcase_your_dance_moves!.py
@@ -0,0 +1,19 @@
+# Question 1 Task 1
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Question 1 Task 2
+
+userFeeling = input("How do you feel today?")
+
+if userFeeling == "happy":
+    print("That's great to hear!")
+elif userFeeling == "sad":
+    print("I hope your day gets better!")
+else:
+    print("I am not sure how to interpret your mood.")
